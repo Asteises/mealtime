4e810ff4d1b5d4be930166299594cf99c877a757
Asteises <p.nikitaev@gmail.com>
2024-03-30 19:54:02 +0300

    add builder to IngredientCategoryInnerDto

--- recipe-service/src/main/java/ru/asteises/recipe_service/ingredient_category/model/dto/IngredientCategoryInnerDto.java
+++ recipe-service/src/main/java/ru/asteises/recipe_service/ingredient_category/model/dto/IngredientCategoryInnerDto.java
@@ -1,11 +1,13 @@
 package ru.asteises.recipe_service.ingredient_category.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @ToString
 @Getter
 @Setter
